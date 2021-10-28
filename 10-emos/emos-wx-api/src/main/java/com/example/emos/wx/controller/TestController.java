4555780da069c454b3c52ff6539f8b7ca0fe04bc
szlsay <13865738+szlsay@users.noreply.github.com>
2021-10-28 11:55:21 +0800

    form-hello

--- 10-emos/emos-wx-api/src/main/java/com/example/emos/wx/controller/TestController.java
+++ 10-emos/emos-wx-api/src/main/java/com/example/emos/wx/controller/TestController.java
@@ -1,22 +1,12 @@
 package com.example.emos.wx.controller;
 
-//import com.example.emos.wx.common.util.R;
-//import com.example.emos.wx.controller.form.TestSayHelloForm;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
-//import org.apache.shiro.authz.annotation.Logical;
-//import org.apache.shiro.authz.annotation.RequiresPermissions;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.validation.Valid;
-
 import com.example.emos.wx.common.util.R;
+import com.example.emos.wx.controller.form.TestSayHelloForm;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/test")
@@ -24,12 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @PostMapping("/sayHello")
     @ApiOperation("最简单的测试方法")
-    public R sayHello(){
-        return R.ok().put("message", "HelloWorld");
+    public R sayHello(@Valid @RequestBody TestSayHelloForm form) {
+        return R.ok().put("message", "Hello,"+form.getName());
     }
-//    public R sayHello(@Valid @RequestBody TestSayHelloForm form) {
-//        return R.ok().put("message", "Hello,"+form.getName());
-//    }
 
     @GetMapping("/hi")
     public R hi(){
