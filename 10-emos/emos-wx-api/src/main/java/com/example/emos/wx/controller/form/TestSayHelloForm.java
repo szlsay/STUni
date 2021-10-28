101eecc359c0dc304cf705cc81782245e81e0833
szlsay <13865738+szlsay@users.noreply.github.com>
2021-10-28 17:51:28 +0800

    wx-init

--- 10-emos/emos-wx-api/src/main/java/com/example/emos/wx/controller/form/TestSayHelloForm.java
+++ 10-emos/emos-wx-api/src/main/java/com/example/emos/wx/controller/form/TestSayHelloForm.java
@@ -10,8 +10,8 @@ import javax.validation.constraints.Pattern;
 @ApiModel
 @Data
 public class TestSayHelloForm {
-//    @NotBlank
-//    @Pattern(regexp = "^[\\u4e00-\\u9fa5]{2,15}$",message = "不符合正则表达式")
+    @NotBlank
+    @Pattern(regexp = "^[\\u4e00-\\u9fa5]{2,15}$",message = "不符合正则表达式")
     @ApiModelProperty("姓名：2-15中文")
     private String name;
 }
