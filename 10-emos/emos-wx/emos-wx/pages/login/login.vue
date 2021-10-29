2cc3c61142b55463257f716d1006d37798713284
szlsay <13865738+szlsay@users.noreply.github.com>
2021-10-29 10:15:39 +0800

    login-ui

---
+++ 10-emos/emos-wx/emos-wx/pages/login/login.vue
@@ -0,0 +1,51 @@
+<template>
+	<view>
+		<image class="logo" src="../../static/logo-1.png" mode="widthFix"></image>
+		<view class="logon-title">EMOS企业在线办公系统</view>
+		<view class="logon-subtitle">Ver 1.0</view>
+		<button class="login-btn" open-type="getUserInfo" @tap="login">登录系统</button>
+		<view class="register-container">
+			没有账号？
+			<text class="register">立即注册</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			login: function(){
+				let that = this;
+				uni.login({
+					provider:"weixin",
+					success:function(resp){
+						// uni.showModal({
+						// 	content: resp.code,
+						// 	showCancel: false
+						// });
+						// that.ajax(that.url.login, "POST", {"code": resp.code}, function(resp){
+						// 	let permission = resp.data.permission
+						// 	uni.showToast({
+						// 		title: permission
+						// 	});
+						// })
+					},
+					fail:function(e){
+						uni.showToast({
+							title: '执行异常'
+						});
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="less">
+@import url("login.less");
+</style>
---
+++ 10-emos/emos-wx/emos-wx/pages/register/register.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<button>button</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style lang="less">
+@import url("register.less");
+</style>
