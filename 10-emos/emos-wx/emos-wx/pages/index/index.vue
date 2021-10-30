70addab784ab75ecb71a3bed099fd7564f4981d5
szlsay <13865738+szlsay@users.noreply.github.com>
2021-10-30 10:49:00 +0800

    banner

--- 10-emos/emos-wx/emos-wx/pages/index/index.vue
+++ 10-emos/emos-wx/emos-wx/pages/index/index.vue
@@ -1,8 +1,11 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<view class="page">
+			<swiper class="swiper" autoplay="true" interval="3000" duration="1000"  circular="true">
+				<swiper-item v-for="imgurl in imgurls" v-key="index">
+					<image :src="imgurl" mode="widthFix"></image>
+				</swiper-item>
+			</swiper>
 		</view>
 	</view>
 </template>
@@ -11,7 +14,12 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				imgurls: ["https://szlsay-1301558572.cos.ap-beijing.myqcloud.com/emos/img/banner/swiper-1.jpg",
+				"https://szlsay-1301558572.cos.ap-beijing.myqcloud.com/emos/img/banner/swiper-2.jpg",
+				"https://szlsay-1301558572.cos.ap-beijing.myqcloud.com/emos/img/banner/swiper-3.jpg",
+				"https://szlsay-1301558572.cos.ap-beijing.myqcloud.com/emos/img/banner/swiper-4.jpg",
+				"https://szlsay-1301558572.cos.ap-beijing.myqcloud.com/emos/img/banner/swiper-5.jpg"]
 			}
 		},
 		onLoad() {
@@ -23,30 +31,6 @@
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang="less">
+	@import url("index.less");
 </style>
