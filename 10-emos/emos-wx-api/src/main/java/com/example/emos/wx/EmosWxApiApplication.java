156544a5f7ddcbc1f5f88e580eafecfac25d4360
szlsay <13865738+szlsay@users.noreply.github.com>
2021-11-02 15:55:32 +0800

    imagefolder

--- 10-emos/emos-wx-api/src/main/java/com/example/emos/wx/EmosWxApiApplication.java
+++ 10-emos/emos-wx-api/src/main/java/com/example/emos/wx/EmosWxApiApplication.java
@@ -6,11 +6,13 @@ import com.example.emos.wx.db.dao.SysConfigDao;
 import com.example.emos.wx.db.pojo.SysConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
 import java.lang.reflect.Field;
 import java.util.List;
 
@@ -24,6 +26,9 @@ public class EmosWxApiApplication {
 
     @Autowired
     private SystemConstants constants;
+
+    @Value("${emos.image-folder}")
+    private String imageFolder;
 
     public static void main(String[] args) {
         SpringApplication.run(EmosWxApiApplication.class, args);
@@ -44,6 +49,7 @@ public class EmosWxApiApplication {
                 log.error("执行异常",e);
             }
         });
+        new File(imageFolder).mkdirs();
     }
 
 }
